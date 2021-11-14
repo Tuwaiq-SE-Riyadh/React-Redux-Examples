d482577ed0498b1fb8be4ebf8e84abcdb95ced94
Reemaioa <eemaibrahim0@hotmail.com>
2021-11-14 16:28:10 +0300

    added

---
+++ redux/src/reducers/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from "redux";
+
+import counter from "./counter/counter";
+// { user } === { user: user }
+const reducers = combineReducers({ counter });
+
+const store = createStore(reducers);
+
+export default store;
